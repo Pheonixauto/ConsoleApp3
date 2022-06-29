82b726ea70e2cd4fd4b13cc0f3d3c1f5cc2aba95
ATS <ATS@DESKTOP-M0SO30Q>
2022-06-29 15:24:31 +0700

    Add project files.

---
+++ ConsoleApp3/Class1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public class Class1
+    {
+        public Class1()
+        {
+
+        }
+        int[] number = { 1, 2, 3, 4, 4, 3, 4, 7, 5, 9, 8, 11,34, 35,35 };
+
+        int a = 0;
+        int b = 0;
+
+        public void cacula()
+        {
+            foreach(int i in number) // Tìm số lớn nhất
+            {
+                if(i!=0 && i > a)
+                {
+                    a = i;
+                    
+                }
+            }
+            foreach(int i in number) // Tìm số lớn thứ 2
+            {
+                if (i!=0 && i > b && i!=a)
+                {
+                   b= i;
+                }
+            }         
+            Console.WriteLine($"SecondMax is : {b}");
+        }
+
+
+        public void IfElse()
+        {
+            for(int i = 0; i <=5; i++)
+            {
+                if (i == 4)
+                {
+                    continue;
+                }
+                Console.WriteLine(i); // in ra 0,1,2,3,5
+            }
+        }
+
+
+     
+       
+
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp3;
+
+Console.WriteLine("Hello, World!");
+
+Class1 Class1 = new Class1();
+Class1.cacula();
+Class1.IfElse();
